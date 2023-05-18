c6d0e59e5510325ab94cc21722035c6604596f45
Manjeet Pathak <manjeetpathak2003@gmail.com>
2023-05-18 14:56:59 +0530

    tailwind initiated

--- alumni-frontend/src/index.js
+++ alumni-frontend/src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css'
-import Router from './utils/Router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import Router from "./utils/Router";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router />
-);
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Router />);
--- alumni-frontend/src/utils/Router.js
+++ alumni-frontend/src/utils/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react";
 
-import Home from '../pages/Home';
+import Home from "../pages/Home";
 
 export default function Router() {
   return (
