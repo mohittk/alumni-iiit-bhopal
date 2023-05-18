da0645bb451fc3d0b79442dc4031f2d4102f78e8
Aman Gupta <ag210211@gmail.com>
2023-05-19 01:28:07 +0530

    feat: Card & Meetup & QueryForm

--- alumni-frontend/src/pages/Home.js
+++ alumni-frontend/src/pages/Home.js
@@ -1,12 +1,38 @@
-import React from "react";
 import Card from "../components/Card";
+import QueryForm from '../components/QueryForm'
+import Meetup from '../components/Meetup'
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, Pagination } from "swiper";
 
-export default function Home() {
+export default function App() {
   return (
     <>
-      <div className="text-[#A020F0]">
-        <Card />
-      </div>
+      {/* <div className="p-[100px]">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide><Card/></SwiperSlide>
+          <SwiperSlide><Card/></SwiperSlide>
+          <SwiperSlide><Card/></SwiperSlide>
+          <SwiperSlide><Card/></SwiperSlide>
+          <SwiperSlide><Card/></SwiperSlide>
+          <SwiperSlide><Card/></SwiperSlide>
+        </Swiper>
+      </div> */}
+      {/* <Meetup/> */}
+
+      <QueryForm/>
     </>
   );
 }
