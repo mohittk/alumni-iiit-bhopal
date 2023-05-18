228de9c254f77fd6c6b08e3637f1f6736f0380e5
mohit khairnar <mvk1407@gmail.com>
2023-05-18 14:33:16 +0530

    feat: added basic folder structure

---
+++ alumni-frontend/src/utils/Router.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react";
+
+import Home from '../pages/Home';
+
+export default function Router() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
