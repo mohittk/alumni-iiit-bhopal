90f2f7cfa606e10a2b34c84ed2e72284a3c665c3
mohit khairnar <mvk1407@gmail.com>
2023-05-20 21:03:30 +0530

    feat: added alumni directory

--- alumni-frontend/src/pages/alumni/AlumniDirectory.js
+++ alumni-frontend/src/pages/alumni/AlumniDirectory.js
@@ -1,10 +1,50 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import mentorPoster from "../../assets/mentor-poster.jpg"
+import { useState, useEffect } from "react";
+import { ThreeCircles } from "react-loader-spinner";
 
-export default function AlumiDirectory() {
+export default function AlumniDirectory() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+     {isLoading ?  <>
+        <div className="h-screen flex items-center justify-center">
+        <ThreeCircles
+          height="200"
+          width="200"
+          color="#2a004f"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="three-circles-rotating"
+          outerCircleColor=""
+          innerCircleColor=""
+          middleCircleColor=""
+        /></div>
+        </> : <>
+        <Navbar />
+      <div class="msgDirectorBanner">
+        <img
+          src={mentorPoster}
+          style={{ filter: "blur(3px)", width: "100%"}}
+        />
+        <div class="centerMsgDirector font-opensans p-4 bg-[#2a004f] ">
+          Alumni Directory
+        </div>
+      </div>
+
+      
+      
+      <Footer /></>}
+     
+    </>
   );
 }
