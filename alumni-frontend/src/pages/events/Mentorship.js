fd3f37f61d645a6d0feffd2bba29151f17b0e9d6
mohit khairnar <mvk1407@gmail.com>
2023-05-20 12:18:13 +0530

    feat: added mentorship page

--- alumni-frontend/src/pages/events/Mentorship.js
+++ alumni-frontend/src/pages/events/Mentorship.js
@@ -1,10 +1,29 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import mentorPoster from "../../assets/mentor-poster.jpg"
 
-export default function Mentorship() {
+export default function About() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <div class="msgDirectorBanner">
+        <img
+          src={mentorPoster}
+          style={{ filter: "blur(3px)", width: "100%"}}
+        />
+        <div class="centerMsgDirector font-opensans p-4 bg-[#2a004f] ">
+          Mentorship Programs
+        </div>
+      </div>
+
+      <div className="mentorship-data m-5 p-5">
+        <h2 className="font-opensans font-bold text-[#2a004f] text-center text-[2.5rem]">
+          IIIT Bhopal Alumni - Student Mentorship Programs
+        </h2>
+      </div>
+      
+      <Footer />
+    </>
   );
 }
